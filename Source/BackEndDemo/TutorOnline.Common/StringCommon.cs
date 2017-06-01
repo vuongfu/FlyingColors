0273a0596f2f495be5dc4807289002eda67c0658
Tran Viet Vuong <vuongfu95@gmail.com>
2017-06-01 22:15:19 +0700

    Vuong add Common library

---
+++ Source/BackEndDemo/TutorOnline.Common/StringCommon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutorOnline.Common
+{
+    public class StringCommon
+    {
+        public string addSubjectSucessfully = "Thêm môn học mới thành công.";
+    }
+}
