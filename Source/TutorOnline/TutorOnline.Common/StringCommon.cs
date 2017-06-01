1d9aeb8802393a2eaa100b7e84533cf466d4d9b5
Tran Viet Vuong <vuongfu95@gmail.com>
2017-06-01 23:26:09 +0700

    vuong add common libarary

---
+++ Source/TutorOnline/TutorOnline.Common/StringCommon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorOnline.Common
+{
+    public class StringCommon
+    {
+        public string addSubjectSuccessfully = "Them mon hoc moi thanh cong.";
+    }
+}
