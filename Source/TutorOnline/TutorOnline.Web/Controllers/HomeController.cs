412d3eb7ee57ef23704e3d193447a189c266f1ea
TheLam <ngoisaoden9@gmai.com>
2017-06-05 23:53:20 +0700

    LamVT
    update router

--- Source/TutorOnline/TutorOnline.Web/Controllers/HomeController.cs
+++ Source/TutorOnline/TutorOnline.Web/Controllers/HomeController.cs
@@ -8,33 +8,7 @@ namespace TutorOnline.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult TrangChu()
-        {
-            return View();
-        }
-
-        public ActionResult GioiThieu()
-        {
-            return View();
-        }
-
-        public ActionResult KhoaHoc()
-        {
-            return View();
-        }
-        public ActionResult GiaSu()
-        {
-            return View();
-        }
-        public ActionResult HuongDan()
-        {
-            return View();
-        }
-        public ActionResult HoTro()
-        {
-            return View();
-        }
-        public ActionResult CongGiaSu()
+        public ActionResult Index()
         {
             return View();
         }
