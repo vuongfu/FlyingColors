152000aeb48f847f6a55998699d3e4492afd2f11
Tran Viet Vuong <vuongfu95@gmail.com>
2017-06-08 05:17:58 +0700

    VuongTV

--- Source/TutorOnline/TutorOnline.Web/Controllers/ManagerController.cs
+++ Source/TutorOnline/TutorOnline.Web/Controllers/ManagerController.cs
@@ -91,7 +91,8 @@ namespace TutorOnline.Web.Controllers
                 manager.BMemName = user.BMemName;
                 manager.ParentID = user.ParentID;
                 manager.Password = user.Password;
-
+                manager.NewPassword = user.Password;
+                manager.ConfirmPassword = user.Password;
             }
             return View(manager);
         }
@@ -130,6 +131,87 @@ namespace TutorOnline.Web.Controllers
             }
             return View(manager);
         }
+
+        public ActionResult ChangePwd(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = MRes.Find(id);
+            PersonalViewModel manager = new PersonalViewModel();
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                manager.Id = user.Id;
+                manager.RoleID = user.RoleID;
+                manager.FirstName = user.FirstName;
+                manager.LastName = user.LastName;
+                manager.Username = user.Username;
+                manager.BirthDate = user.BirthDate;
+                manager.Gender = user.Gender;
+                manager.Address = user.Address;
+                manager.Email = user.Email;
+                manager.SkypeID = user.SkypeID;
+                manager.City = user.City;
+                manager.PostalCode = user.PostalCode;
+                manager.Country = user.Country;
+                manager.PhoneNumber = user.PhoneNumber;
+                manager.Photo = user.Photo;
+                manager.Description = user.Description;
+                manager.Wallet = user.Wallet;
+                manager.Salary = user.Salary;
+                manager.BankID = user.BankID;
+                manager.BankName = user.BankName;
+                manager.BMemName = user.BMemName;
+                manager.ParentID = user.ParentID;
+                manager.Password = user.Password;
+                manager.NewPassword = user.Password;
+                manager.ConfirmPassword = user.Password;
+            }
+            return View(manager);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangePwd(PersonalViewModel manager)
+        {
+            User user = new User();
+            user.Id = manager.Id;
+            user.FirstName = manager.FirstName;
+            user.LastName = manager.LastName;
+            user.Username = manager.Username;
+            user.Password = manager.NewPassword;
+            user.ParentID = manager.ParentID;
+            user.RoleID = manager.RoleID;
+            user.BirthDate = manager.BirthDate;
+            user.Gender = manager.Gender;
+            user.Address = manager.Address;
+            user.Email = manager.Email;
+            user.SkypeID = manager.SkypeID;
+            user.City = manager.City;
+            user.PostalCode = manager.PostalCode;
+            user.Country = manager.Country;
+            user.PhoneNumber = manager.PhoneNumber;
+            user.Photo = manager.Photo;
+            user.Description = manager.Description;
+            user.BankID = manager.BankID;
+            user.BankName = manager.BankName;
+            user.BMemName = manager.BMemName;
+
+            if (ModelState.IsValid)
+            {
+                MRes.Edit(user);
+                TempData["message"] = "Bạn đã đổi mật khẩu thành công";
+                return RedirectToAction("ChangePwd", new { id = user.Id });
+            }
+            return View(manager);
+        }
+
+
 
         protected override void Dispose(bool disposing)
         {
--- Source/TutorOnline/TutorOnline.Web/Models/ManageViewModels.cs
+++ Source/TutorOnline/TutorOnline.Web/Models/ManageViewModels.cs
@@ -31,6 +31,16 @@ namespace TutorOnline.Web.Models
         public string Description { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "NewPassword")]
+        public string NewPassword { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "ConfirmPassword")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
         public string BankName { get; set; }
         public string BMemName { get; set; }
     }
