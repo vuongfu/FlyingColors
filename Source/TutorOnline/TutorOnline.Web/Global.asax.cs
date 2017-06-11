b6e87b63c58fe19d76d784da2c66c1fb37c15c63
TheLam <ngoisaoden9@gmai.com>
2017-06-11 22:16:33 +0700

    delete startup.cs

--- Source/TutorOnline/TutorOnline.Web/Global.asax.cs
+++ Source/TutorOnline/TutorOnline.Web/Global.asax.cs
@@ -13,7 +13,6 @@ namespace TutorOnline.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
