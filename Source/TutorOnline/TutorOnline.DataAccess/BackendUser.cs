58589d347cc81f2cff327dcbff3d43a94d1a7f5d
TheLam <ngoisaoden9@gmai.com>
2017-07-05 00:00:06 +0700

    update DB entities

--- Source/TutorOnline/TutorOnline.DataAccess/BackendUser.cs
+++ Source/TutorOnline/TutorOnline.DataAccess/BackendUser.cs
@@ -27,7 +27,7 @@ namespace TutorOnline.DataAccess
         public string City { get; set; }
         public string Country { get; set; }
         public string PhoneNumber { get; set; }
-        public byte[] Photo { get; set; }
+        public string Photo { get; set; }
         public string Description { get; set; }
         public bool isDeleted { get; set; }
     
