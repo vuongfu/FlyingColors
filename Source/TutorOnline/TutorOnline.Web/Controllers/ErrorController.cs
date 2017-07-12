406853cf2a6836a7ac643e6ae2eab5dcc8d62f5d
Tran Viet Vuong <vuongfu95@gmail.com>
2017-07-12 22:46:28 +0700

    vuongtv - update

--- Source/TutorOnline/TutorOnline.Web/Controllers/ErrorController.cs
+++ Source/TutorOnline/TutorOnline.Web/Controllers/ErrorController.cs
@@ -9,13 +9,6 @@ namespace TutorOnline.Web.Controllers
 {
     public class ErrorController : Controller
     {
-        public ActionResult servererror()
-        {
-            Response.TrySkipIisCustomErrors = true;
-            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            return View();
-        }
-
         public ActionResult notfound()
         {
             Response.TrySkipIisCustomErrors = true;
