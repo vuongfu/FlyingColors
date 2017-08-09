cddfa7b42d7379b708b2ff82533a289ac19edbd4
TheLam <ngoisaoden9@gmai.com>
2017-08-09 12:23:05 +0700

    update

--- Source/TutorOnline/TutorOnline.Business/Repository/AccountRepository.cs
+++ Source/TutorOnline/TutorOnline.Business/Repository/AccountRepository.cs
@@ -11,22 +11,40 @@ namespace TutorOnline.Business.Repository
     {
         public string Authenticate(string username, string password)
         {
-            var CheckBackEndUser = _dbContext.BackendUsers.FirstOrDefault(a => a.UserName == username && String.Compare(a.Password, password, false) == 0);
-            var CheckTutor = _dbContext.Tutors.FirstOrDefault(a => a.UserName == username && String.Compare(a.Password, password, false) == 0);
-            var CheckStudent = _dbContext.Students.FirstOrDefault(a => a.UserName == username && String.Compare(a.Password, password, false) == 0);
-            var CheckParent = _dbContext.Parents.FirstOrDefault(a => a.UserName == username && String.Compare(a.Password, password, false) == 0);
+            var CheckBackEndUser = _dbContext.BackendUsers.FirstOrDefault(a => a.UserName == username);
+            var CheckTutor = _dbContext.Tutors.FirstOrDefault(a => a.UserName == username);
+            var CheckStudent = _dbContext.Students.FirstOrDefault(a => a.UserName == username);
+            var CheckParent = _dbContext.Parents.FirstOrDefault(a => a.UserName == username);
             if (CheckBackEndUser == null && CheckTutor == null && CheckStudent == null && CheckParent == null)
                 return null;
 
             string returnRoleName = null;
+            
             if (CheckBackEndUser != null)
-                returnRoleName = CheckBackEndUser.Role.RoleName;
-            if (CheckTutor != null)
-                returnRoleName = CheckTutor.Role.RoleName;
+            {
+                string pass = CheckBackEndUser.Password;
+                if (pass.Equals(password,StringComparison.CurrentCulture))
+                    returnRoleName = CheckBackEndUser.Role.RoleName;
+                
+            }                
+            if (CheckTutor != null) {
+                string pass = CheckTutor.Password;
+                if (pass.Equals(password, StringComparison.CurrentCulture))
+                    returnRoleName = CheckTutor.Role.RoleName;
+            }                
             if (CheckStudent != null)
-                returnRoleName = CheckStudent.Role.RoleName;
+            {
+                string pass = CheckStudent.Password;
+                if (pass.Equals(password, StringComparison.CurrentCulture))
+                    returnRoleName = CheckStudent.Role.RoleName;
+            }                
             if (CheckParent != null)
-                returnRoleName = CheckParent.Role.RoleName;
+            {
+                string pass = CheckParent.Password;
+                if (pass.Equals(password, StringComparison.CurrentCulture))
+                    returnRoleName = CheckParent.Role.RoleName;
+            }
+                
 
             return returnRoleName;
         }
