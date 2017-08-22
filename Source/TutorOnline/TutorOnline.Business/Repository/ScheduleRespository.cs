47bb0c8e260cb26ebcc9330c0e1687ae8c8e5945
TheLam <ngoisaoden9@gmai.com>
2017-08-22 22:02:02 +0700

    update

--- Source/TutorOnline/TutorOnline.Business/Repository/ScheduleRespository.cs
+++ Source/TutorOnline/TutorOnline.Business/Repository/ScheduleRespository.cs
@@ -81,6 +81,7 @@ namespace TutorOnline.Business.Repository
             {
                 slot.Status = 5;
                 slot.CanReason = reason;
+                slot.StudentId = null;
                 _dbContext.Entry(slot).State = EntityState.Modified;
                 _dbContext.SaveChanges();
             }
